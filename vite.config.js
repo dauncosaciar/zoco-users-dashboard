6da62d495f662dc5b8acbea8b9549bfa54d0534f
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-05-07 17:25:58 -0300

    install sass, add styles folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        sourceMapIncludeSources: true
+      }
+    }
+  },
   server: {
     port: 9999
   }
