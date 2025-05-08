5bea9eabcd9215a727a4e299d7ff02f75053dadc
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-05-08 14:51:29 -0300

    add AuthContext and axiosClient

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,3 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 export default function AuthLayout() {
-  return <div className="auth-layout">AuthLayout</div>;
+  return (
+    <div className="auth-layout">
+      <Outlet />
+    </div>
+  );
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
 import AuthLayout from "./layouts/AuthLayout";
 import AppLayout from "./layouts/AppLayout";
 import LoginView from "./views/auth/LoginView";
@@ -7,15 +8,17 @@ import DashboardView from "./views/users/DashboardView";
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<LoginView />} />
-        </Route>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<LoginView />} />
+          </Route>
 
-        <Route path="/dashboard" element={<AppLayout />}>
-          <Route index element={<DashboardView />} />
-        </Route>
-      </Routes>
+          <Route path="/dashboard" element={<AppLayout />}>
+            <Route index element={<DashboardView />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
