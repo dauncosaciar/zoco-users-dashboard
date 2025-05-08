88c0d7e2556f6807f20718ed0e6c827c782bc796
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-05-08 16:09:57 -0300

    add login method in AuthProvider

---
+++ src/api/AuthApi.js
@@ -0,0 +1,9 @@
+import axiosClient from "../config/axiosClient";
+
+export async function loginUser(email, password) {
+  const url = `/users?email=${email}&password=${password}`;
+  const { data } = await axiosClient.get(url);
+  const user = data[0];
+
+  return user || null;
+}
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,70 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../api/authApi";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [role, setRole] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedToken = sessionStorage.getItem("token");
+    const storedUser = sessionStorage.getItem("user");
+    const storedRole = sessionStorage.getItem("role");
+
+    if (storedToken && storedUser) {
+      setToken(JSON.parse(storedToken));
+      setUser(JSON.parse(storedUser));
+      setRole(JSON.parse(storedRole));
+    }
+  }, []);
+
+  const login = async (email, password) => {
+    try {
+      const user = await loginUser(email, password);
+
+      if (!user) return false;
+
+      const { password: pwd, ...userWithoutPassword } = user;
+      const token = "token__" + Date.now();
+
+      setToken(token);
+      setUser(userWithoutPassword);
+      setRole(userWithoutPassword.role);
+
+      sessionStorage.setItem("token", JSON.stringify(token));
+      sessionStorage.setItem("user", JSON.stringify(userWithoutPassword));
+      sessionStorage.setItem("role", JSON.stringify(userWithoutPassword.role));
+
+      navigate("/dashboard");
+      return true;
+    } catch (error) {
+      console.error("Login error:", error);
+      return false;
+    }
+  };
+
+  const isAuthenticated = !!token;
+
+  const logout = () => {};
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        user,
+        role,
+        isAuthenticated,
+        login,
+        logout
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthProvider };
